a64314e4aa9967ce596103c17df8401210ff78c6
Artur Ilkaev <xaysdiablo@gmail.com>
2019-05-03 12:54:41 +0500

    AddWater code

--- app/src/main/java/com/s32xlevel/foodtracker/controller/AddWaterFragment.kt
+++ app/src/main/java/com/s32xlevel/foodtracker/controller/AddWaterFragment.kt
@@ -7,16 +7,26 @@ import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.*
+import android.widget.RadioButton
+import com.s32xlevel.foodtracker.AuthorizedUser
 import com.s32xlevel.foodtracker.R
+import com.s32xlevel.foodtracker.model.Water
+import com.s32xlevel.foodtracker.repository.WaterRepository
+import com.s32xlevel.foodtracker.repository.WaterRepositoryImpl
+import kotlinx.android.synthetic.main.fragment_add_water.*
+import kotlinx.android.synthetic.main.fragment_settings.*
+import org.joda.time.DateTime
 
 
 class AddWaterFragment : Fragment() {
 
     var listenerChange: ChangeFragment? = null
+    var waterRepository: WaterRepository? = null
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
         listenerChange = context as ChangeFragment
+        waterRepository = WaterRepositoryImpl(context)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -29,6 +39,12 @@ class AddWaterFragment : Fragment() {
         return rootView
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkMyButtonSource()
+        onClickButton()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
         inflater!!.inflate(R.menu.main_menu, menu)
     }
@@ -38,6 +54,40 @@ class AddWaterFragment : Fragment() {
             R.id.menu_settings -> listenerChange!!.changeToSettings()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun checkMyButtonSource() {
+        val button = view!!.findViewById<RadioButton>(radio_group_add_water.checkedRadioButtonId)
+        if (button.id == radio_button_mysource.id) {
+            fill_water_my_source.visibility = View.VISIBLE
+            fill_water_my_volume.visibility = View.VISIBLE
+        } else {
+            fill_water_my_source.visibility = View.INVISIBLE
+            fill_water_my_volume.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun onClickButton() {
+        button_add_water.setOnClickListener {
+            val button = view!!.findViewById<RadioButton>(radio_group_add_water.checkedRadioButtonId)
+            when (button.id) {
+                radio_button_cup.id -> {
+                    addWater("Стакан", 200)
+                }
+                radio_button_mug.id -> {
+                    addWater("Кружка", 300)
+                }
+                radio_button_mysource.id -> {
+                    addWater(fill_water_my_source.text.toString(), fill_water_my_volume.text.toString().toInt())
+                }
+            }
+        }
+    }
+
+    private fun addWater(source: String, volume: Int) {
+        val water = Water(id = null, dateTime = DateTime().toString("yyyy-MM-ddTHH:mm"),
+            volume = volume, source = source, userId = AuthorizedUser.id)
+        waterRepository!!.create(water, AuthorizedUser.id)
     }
 
 
