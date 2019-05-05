f9384944a08599a0949a3c031f8b6b2fc586b57e
Artur Ilkaev <xaysdiablo@gmail.com>
2019-05-05 19:24:53 +0500

    FoodAdapter

--- app/src/main/java/com/s32xlevel/foodtracker/util/FoodUtil.kt
+++ app/src/main/java/com/s32xlevel/foodtracker/util/FoodUtil.kt
@@ -18,8 +18,8 @@ class FoodUtil {
                     FoodType(id = 1, typeName = "Первый завтрак", timeReception = convertStringTimeToDateTime(user.startDayTime!!)),
                     FoodType(id = 2, typeName = "Второй завтрак", timeReception = convertStringTimeToDateTime("11:00:00")),
                     FoodType(id = 3, typeName = "Обед", timeReception = convertStringTimeToDateTime("14:30:00")),
-                    FoodType(id = 5, typeName = "Первый ужин", timeReception = convertStringTimeToDateTime("19:00:00")),
-                    FoodType(id = 6, typeName = "Второй ужин", timeReception = convertStringTimeToDateTime("20:30:00")))
+                    FoodType(id = 4, typeName = "Первый ужин", timeReception = convertStringTimeToDateTime("19:00:00")),
+                    FoodType(id = 5, typeName = "Второй ужин", timeReception = convertStringTimeToDateTime("20:30:00")))
             }
             return null
         }
---
+++ app/src/main/java/com/s32xlevel/foodtracker/util/RecyclerFoodAdapter.kt
@@ -0,0 +1,48 @@
+package com.s32xlevel.foodtracker.util
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.s32xlevel.foodtracker.R
+import com.s32xlevel.foodtracker.repository.UserRepositoryImpl
+
+class RecyclerFoodAdapter(val context: Context) : RecyclerView.Adapter<RecyclerFoodAdapter.ViewHolder>() {
+
+    interface Listener {
+        fun onClick()
+    }
+
+    var listener: Listener? = null
+
+    private val user = UserRepositoryImpl(context).findById(1)
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var typeName = itemView.findViewById<TextView>(R.id.food_recycler_typeName)
+        var time = itemView.findViewById<TextView>(R.id.food_recycler_time)
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.recycler_view_food, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val dishes = FoodUtil.getFoodsForUser(user!!)
+        val dish = dishes!![position]
+
+        val addedTime = dish.timeReception!!.plusMinutes(30)
+        val time = "${dish.timeReception!!.toLocalTime().toString("HH:mm")} - ${addedTime.toLocalTime().toString("HH:mm")}"
+
+        viewHolder.typeName.text = dish.typeName
+        viewHolder.typeName.text = time
+
+        viewHolder.itemView.setOnClickListener { listener!!.onClick() }
+    }
+
+    override fun getItemCount(): Int {
+        return FoodUtil.getFoodsForUser(user!!)!!.size
+    }
+}
